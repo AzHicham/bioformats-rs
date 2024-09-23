9a018d4a2c512527190c1a49dcc8e984660b2855
AzHicham <hicham.azimani@owkin.com>
2024-09-23 17:38:14 +0200

    fix: add retry in build.rs

--- build.rs
+++ build.rs
@@ -1,19 +1,28 @@
+use j4rs::errors::J4RsError;
 use j4rs::{JvmBuilder, MavenArtifact, MavenArtifactRepo, MavenSettings};
+use retry::delay::Exponential;
+use retry::{delay, retry};
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     println!("cargo:rerun-if-changed=build.rs");
 
+    Ok(retry(
+        Exponential::from_millis(1000).map(delay::jitter).take(4),
+        deploy_java_artifacts,
+    )?)
+}
+
+fn deploy_java_artifacts() -> Result<(), J4RsError> {
     let jvm = JvmBuilder::new()
         .with_maven_settings(MavenSettings::new(vec![MavenArtifactRepo::from(
             "openmicroscopy::https://artifacts.openmicroscopy.org/artifactory/ome.releases",
         )]))
-        .build()
-        .expect("Failed to build JVM");
+        .build()?;
 
-    jvm.deploy_artifact(&MavenArtifact::from("ome:bioformats_package:7.3.1"))
-        .expect("Failed to deploy artifact bioformats_package");
+    jvm.deploy_artifact(&MavenArtifact::from("ome:bioformats_package:7.3.1"))?;
 
     #[cfg(feature = "gpl-formats")]
-    jvm.deploy_artifact(&MavenArtifact::from("ome:formats-gpl:7.3.1"))
-        .expect("Failed to deploy artifact formats-gpl");
+    jvm.deploy_artifact(&MavenArtifact::from("ome:formats-gpl:7.3.1"))?;
+
+    Ok(())
 }
